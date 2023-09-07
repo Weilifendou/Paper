56c3f4bac8e87dac8526e56b9ec9575f85f44972
weilifendou <hql4072@qq.com>
2023-09-07 16:13:08 +0800

    realize roughly circle detecting

--- Code/PythonProject/DetectBoard/main.py
+++ Code/PythonProject/DetectBoard/main.py
@@ -9,43 +9,81 @@ def drawTrackbar():
     cv2.namedWindow('Main')
     # mat = np.ones((30,975)) #调节Trackbar显示的宽度
     # cv2.imshow('Main', mat)
-    cv2.createTrackbar('GausSize', 'Main', 17, 255, empty)
-    cv2.createTrackbar('CannyThreshold1', 'Main', 46, 255, empty)
-    cv2.createTrackbar('CannyThreshold2', 'Main', 190, 255, empty)
-    cv2.createTrackbar('SobelWeight', 'Main', 50, 100, empty)
+    cv2.createTrackbar('GausSize', 'Main', 20, 255, empty)
+    cv2.createTrackbar('CannyThreshold1', 'Main', 10, 255, empty)
+    cv2.createTrackbar('CannyThreshold2', 'Main', 30, 255, empty)
+    cv2.createTrackbar('Dist', 'Main', 10, 500, empty)
+    cv2.createTrackbar('MinRaduis', 'Main', 10, 500, empty)
+    cv2.createTrackbar('MaxRaduis', 'Main', 50, 500, empty)
 def detectEdge(image):
     while cv2.getWindowProperty('Main', cv2.WND_PROP_VISIBLE) >= 1:
         gausSize = cv2.getTrackbarPos('GausSize', 'Main')
         CannyThreshold1 = cv2.getTrackbarPos('CannyThreshold1', 'Main')
         CannyThreshold2 = cv2.getTrackbarPos('CannyThreshold2', 'Main')
-        SobelWeight = cv2.getTrackbarPos('SobelWeight', 'Main')
-        SobelWeight /= 100
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        ret, img = cv2.threshold(gray, CannyThreshold1, 255, cv2.THRESH_BINARY);
-        medianImg = cv2.medianBlur(img, 5)
-        # 使用Sobel算子进行边缘检测
-        sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, 5)
-        sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, 5)
-
-        # 将梯度值取绝对值
-        sobel_x = cv2.convertScaleAbs(sobel_x)
-        sobel_y = cv2.convertScaleAbs(sobel_y)
-
-        # 合并x和y方向的梯度
-        sobel = cv2.addWeighted(sobel_x, SobelWeight, sobel_y, 1-SobelWeight, 0)
         if gausSize % 2 == 0: gausSize += 1
-        gausBlur = cv2.GaussianBlur(sobel, (gausSize, gausSize), 0)
+        gausBlur = cv2.GaussianBlur(gray, (gausSize, gausSize), 0)
         canny = cv2.Canny(gausBlur, CannyThreshold1, CannyThreshold2)
-        cv2.imshow('Main', medianImg)
+        kernel = np.ones((5, 5), np.uint8)
+        canny = cv2.dilate(canny, kernel, 1)
+        cv2.imshow('Main', canny)
+
+        if cv2.waitKey(1) == 27:
+            break
+def DetectCircle(image):
+    while cv2.getWindowProperty('Main', cv2.WND_PROP_VISIBLE) >= 1:
+        gausSize = cv2.getTrackbarPos('GausSize', 'Main')
+        CannyThreshold1 = cv2.getTrackbarPos('CannyThreshold1', 'Main')
+        CannyThreshold2 = cv2.getTrackbarPos('CannyThreshold2', 'Main')
+        Dist = cv2.getTrackbarPos('Dist', 'Main')
+        MinRaduis = cv2.getTrackbarPos('MinRaduis', 'Main')
+        MaxRaduis = cv2.getTrackbarPos('MaxRaduis', 'Main')
+        origin = np.copy(image)
+        gray = cv2.cvtColor(origin, cv2.COLOR_BGR2GRAY)
+        if gausSize % 2 == 0: gausSize += 1
+        blurred = cv2.GaussianBlur(gray, (gausSize, gausSize), 0)
+        canny = cv2.Canny(blurred, CannyThreshold1, CannyThreshold2)
+        circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1,
+                           Dist, param1=CannyThreshold1, param2=CannyThreshold2, minRadius=MinRaduis, maxRadius=MaxRaduis)
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            for (x, y, r) in circles:
+                cv2.circle(origin, (x, y), r, (0, 255, 0), 2)
+            cv2.imshow("Main", origin)
+        else:
+            print("未检测到圆形")
+
         if cv2.waitKey(1) == 27:
             break
 
+
+def DetectRetangle(image):
+    while cv2.getWindowProperty('Main', cv2.WND_PROP_VISIBLE) >= 1:
+        gausSize = cv2.getTrackbarPos('GausSize', 'Main')
+        CannyThreshold1 = cv2.getTrackbarPos('CannyThreshold1', 'Main')
+        CannyThreshold2 = cv2.getTrackbarPos('CannyThreshold2', 'Main')
+        Dist = cv2.getTrackbarPos('Dist', 'Main')
+        MinRaduis = cv2.getTrackbarPos('MinRaduis', 'Main')
+        MaxRaduis = cv2.getTrackbarPos('MaxRaduis', 'Main')
+        origin = np.copy(image)
+        gray = cv2.cvtColor(origin, cv2.COLOR_BGR2GRAY)
+        if gausSize % 2 == 0: gausSize += 1
+        blurred = cv2.GaussianBlur(gray, (gausSize, gausSize), 0)
+        edges = cv2.Canny(blurred, CannyThreshold1, CannyThreshold2)
+        contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(origin, contours, -1, (0, 0, 255), 2)
+        background = np.ones_like(origin)
+        cv2.drawContours(background, contours, -1, (0, 0, 255), 1)
+        cv2.imshow("Main", background)
+        if cv2.waitKey(1) == 27:
+            break
 def main():
-    image = cv2.imread('3.jpg')
+    image = cv2.imread('1.jpg')
     image = cv2.resize(image, (1200, 800))
     drawTrackbar()
-    detectEdge(image)
-
+    DetectCircle(image)
+    # detectEdge(image)
+    # DetectRetangle(image)
 
 if __name__ == '__main__':
     print('hello world')
